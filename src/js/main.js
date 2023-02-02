999891cef99bfc4b6b9848796f1ad0229d9e5ab3
Fabricio <101414179+FaArMa@users.noreply.github.com>
2023-02-02 12:45:38 -0300

    First "functional and beautiful" version

---
+++ src/js/main.js
@@ -0,0 +1,34 @@
+window.onload = function () {
+    document.getElementById("DspRes").innerHTML = window.screen.width + " x " + window.screen.height;
+    document.getElementById("BrVi").innerHTML = window.innerWidth + " x " + window.innerHeight;
+    document.getElementById("DPR").innerHTML = window.devicePixelRatio.toFixed("2");
+    document.getElementById("UAStr").innerHTML = navigator.userAgent;
+    updateDisOrien();
+    updateUATyp();
+};
+
+function updateDisOrien() {
+    if (window.innerHeight > window.innerWidth)
+        document.getElementById("DisOrien").innerHTML = "Vertical";
+    else
+        document.getElementById("DisOrien").innerHTML = "Horizontal";
+}
+
+function updateUATyp() {
+    let UATyp = "Desktop";
+    if (isMobile())
+        UATyp = "Mobile";
+    if (isTouch())
+        UATyp += " (touch)";
+    else
+        UATyp += " (no touch)";
+    document.getElementById("UATyp").innerHTML = UATyp;
+}
+
+function isMobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+function isTouch() {
+    return (window.hasOwnProperty("ontouchstart") || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0);
+}
