a7bdde877fefec5921e350a5ce8e6f046f5aba26
Fabricio <101414179+FaArMa@users.noreply.github.com>
2023-02-02 19:05:45 -0300

    Modification to use AddEventListener And contemplate the case of a square display

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,23 @@
-window.onload = function () {
+window.addEventListener("load", updateAll);
+window.addEventListener("resize", updateAll);
+window.addEventListener("orientationchange", updateAll);
+
+function updateAll() {
     document.getElementById("DspRes").innerHTML = window.screen.width + " x " + window.screen.height;
     document.getElementById("BrVi").innerHTML = window.innerWidth + " x " + window.innerHeight;
     document.getElementById("DPR").innerHTML = window.devicePixelRatio.toFixed("2");
     document.getElementById("UAStr").innerHTML = navigator.userAgent;
     updateDisOrien();
     updateUATyp();
-};
+}
 
 function updateDisOrien() {
     if (window.innerHeight > window.innerWidth)
         document.getElementById("DisOrien").innerHTML = "Vertical";
-    else
+    else if (window.innerHeight < window.innerWidth)
         document.getElementById("DisOrien").innerHTML = "Horizontal";
+    else
+        document.getElementById("DisOrien").innerHTML = "Square";
 }
 
 function updateUATyp() {
